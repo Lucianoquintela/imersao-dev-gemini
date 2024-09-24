f534bcb55c8e9284510ff9f7d1880c824ba22960
Lucianoquintela <117698113+Lucianoquintela@users.noreply.github.com>
2024-09-24 09:01:46 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -1,312 +1,393 @@
 // Dados dos Mutantes
 export const dadosDosMutantes = {
-    mutante01: {
-    			  	numero: 1,
-        foto: "./assets/vlado.jpeg",
-        nome: "Vlado",
-        nomeCompleto: "Vladmir Pereira",
-        poderes: [
-            "Presas Afiadas",
-            "Hematofagia",
-            "Sentidos Aguçados",
-            "Visão Noturna",
-            "Transmissão de Vampirismo",
-            "Ecolocalização",
-            "Agilidade de Morcego"
-        ]
-    },
-    mutante02: {
-    				   numero:2,
-        foto: "./assets/chris.JPG",
-        nome: "Chris",
-        nomeCompleto: "Cristiano Pena",
-        poderes: [
-    				"Super Sensibilidade",
-    				"Telecinese",
-    				"Sentidos Aguçados",
-    				"Telepatia",  
-    				"Lingüistica Animal",
-    				"Aerocinese",
-    		"Psicocinese",
-    		"Controle de Gravidade",
-    		"Magnetocinese",
-    				"Indução de Dor",
-    				"Ataques Mentais",
-    			"Regeneração Celular",
-    				"Imortalidade",
-    				"Materialização",
-    			"Fotocinese",
-    				"Indução de Morte",
-    				"Absorção de Força Vital",
-    				"Indução de Paralisia",
-    				"Manipulação da Idade",
-    			"Transmutação",
-    				"Controle Climático",
-    				"Anulação de Poderes",
-    				"Super Resistência",
-    				"Pirocinese",
-    				"Envelhecimento Precoce"
-    				
-    				]
-    },
-			  	mutante03: {
-			  				  	numero: 3,
-			  				  	foto: "./assets/agata.jpg",
-			  				  	nome: "Ágata",
-			  				  	nomeCompleto: "Ágata Magalhães",
-        poderes: [
-            "Garras",
-			  				  	     "Super Visão"
-			  				  	]
-			  	},
-			  		mutante04: {
-			  				  	numero: 4,
-			  				  	foto: "./assets/metamorfo.jpeg",
-			  				  	nome: "Metamorfo",
-			  				  	nomeCompleto: "Mateus Morpheus Mayer",
-        poderes: [
-            "Invisibilidade",
-			  					  	"Metamorfismo Corporal"
-			  					  	]
-			  	},
-			  	
-			  		mutante05: {
-			  				  	numero: 5,
-			  				  	foto: "./assets/aquiles.jpeg",
-			  				  	nome: "Aquiles",
-			  				  	nomeCompleto: "Aquiles Magalhães",
-        poderes: [
-            "Super Velocidade",
-			  					  	"Regeneração Celular",
-			  					  	"Imunidade a Contaminantes",
-			  					  	"Super Agilidade",
-			  					  	"Super Resistência"
-			  					  	]
-			  	},
-			  		mutante06: {
-			  				  	numero: 6,
-			  				  	foto: "./assets/clara.jpeg",
-			  				  	nome: "Clara",
-			  				  	nomeCompleto: "Clara Menezes",
-        poderes: [
-            "Cura",
-			  					  	  "Super Sensibilidade",
-			  					  	"Hipnose Animal",
-			  					  	"Luminescência",
-			  					  	"Transmissão de Sentimentos"
-			  					  	]
-			  	},
-			  	mutante07: {
-			  				  	numero: 7,
-			  				  	foto: "./assets/angela.jpeg",
-			  				  	nome: "Ângela",
-			  				  	nomeCompleto: "Ângela Menezes",
-        poderes: [
-            "Asas",
-			  				  	"Dom da Calma",
-			  				  	"Super Sensibilidade"
-			  				  	]
-			  	},
-			  	mutante08: {
-			  				  	numero: 8,
-			  				  	foto: "./assets/tatiana.jpg",
-			  				  	nome: "Tati",
-			  				  	nomeCompleto: "Tatiana Montenegro",
-        poderes: [
-            "Telecinese",
-			  				  	     "Pirocinese",
-			  				  	    "Ondas Mentais",
-			  				  	"Telepatia",
-			  				  	"Tortura mental",
-			  				  	     "Raios de energia",
-			  				  	    "Magnetocinese",
-			  				  	"Grito Super Sônico",
-			  				  	"Fotocinese",
-			  				  	     "Campos de Força",
-			  				  	    "Escudos Mentais",
-			  				  	"Atomocinese",
-			  				  	"Imobilização",
-			  				  	"Geocinese",
-			  				  	"Rastreamento",
-			  				  	     "Sentido de perigo",
-			  				  	    "Areocinese",
-			  				  	"Detectação de Energia",
-			  				  	]
-			  	}	,
-			  	mutante09: {
-			  				  	numero: 9,
-			  				  	foto: "./assets/toni.jpeg",
-			  				  	nome: "Toni",
-			  				  	nomeCompleto: "Antônio Mayer",
-        poderes: [
-            "Super Agilidade",
-			  				  	     "Super Reflexos",
-			  				  	"Super Equilíbrio",
-			  				  	"Super Resistência"
-			  				  	]
-			  	},
-			  	mutante10: {
-			  				  	numero: 10,
-			  				  	foto: "./assets/janete.jpeg",
-			  				  	nome: "Janete",
-			  				  	nomeCompleto: "Janete Fontes Martinelli",
-        poderes: [
-            "Premonição",
-			  				  	"Clarividência",
-			  				  	     "Raios Oculares",
-			  				  	    "Controle Mental",
-			  				  	"Controle Mental",
-			  				  	"Super Sensibilidade",
-			  				  	     "Biocinese",
-			  				  	    "Neutralização de Poderes",
-			  				  	"Reversão de Poderes",
-			  				  	"Manipulação Vital"
-			  				  	]
-			  	},
-			  	mutante11: {
-			  				  	numero: 11,
-			  				  	foto: "./assets/perpetua.jpeg",
-			  				  	nome: "Perpétua",
-			  				  	nomeCompleto: "Perpétua Salvador da Silva",
-        poderes: [
-            "Eletrocinese",
-			  				  	"Campo de Energia Elétrica"
-			  				  	]
-			  	},
-			  	mutante12: {
-			  				  	numero: 12,
-			  				  	foto: "./assets/pachola.jpeg",
-			  				  	nome: "Pachola",
-			  				  	nomeCompleto: "Paulo Pachola",
-        poderes: [
-            "Genes de Lobo",
-			  				  	"Presas Afiadas",
-			  				  	"Sentidos Aguçados",
-			  				  	"Força Sobre-Humana",
-			  				  	"Transmissão de Licantropia"
-			  				  	]
-			  	}	,
-			  	mutante13: {
-			  				  	numero: 13,
-			  				  	foto: "./assets/maria.jpeg",
-			  				  	nome: "Maria Mayer",
-			  				  	nomeCompleto: "Maria Beatriz Montenegro",
-        poderes: [
-            "Super força",
-			  				  	"Super agilidade",
-			  				  	"Resistência evoluída",
-			  				  	"Habilidades acrobáticas",
-			  				  	"Intangibilidade",
-			  				  		"Sensibilidade sobre-humana",
-			  				  	"Manipulação de Feromônios",
-			  				  	"Absorção de poderes",
-			  				  	"Cariocinese",
-			  				  	"Espinhos",
-			  				  		"Magnetocinese",
-			  				  	"Eletrocinese",
-			  				  	]
-			  	}	,
-			  	mutante14: {
-			  				  	numero: 14,
-			  				  	foto: "./assets/vava.jpeg",
-			  				  	nome: "Vavá",
-			  				  	nomeCompleto: "Valfredo Pachola",
-        poderes: [
-			  				  	"Genes de Lobo",
-			  				  	"Transmissão de Licantropia",
-			  				  	"Super força",
-			  				  	"Presas Afiadas",
-			  				  	"Sentidos Aguçados",
-			  				  	"Super Agilidade"
-			  				  	]
-			  	},
-			  	
-			  	mutante15: {
-			  				  	numero: 15,
-			  				  	foto: "./assets/lucas.jpeg",
-			  				  	nome: "Lucas",
-			  				  	nomeCompleto: "Lucas Fontes Martinelli",
-        poderes: [
-            "Ler pensamentos",
-			  				  	
-			  				  	]
-			  	},
-			  	
-			  	mutante16: {
-			  				  	numero: 16,
-			  				  	foto: "./assets/cleo.jpeg",
-			  				  	nome: "Cléo",
-			  				  	nomeCompleto: "Cléo Mayer",
-        poderes: [
-            "Sobrevivência",
-			  				  	     "Projeção Astral",
-			  				  	"Petrificação"
-			  				  	]
-			  	},mutante17: {
-			  				  	numero: 17,
-			  				  	foto: "./assets/dr-cesar.jpeg",
-			  				  	nome: "Dr. césar",
-			  				  	nomeCompleto: "César Rubicão",
-        poderes: [
-            "Genes de Lobo",
-			  				  	"Presas Afiadas",
-			  				  	"Sentidos Aguçados",
-			  				  	"Força Sobre-Humana",
-			  				  	"Transmissão de Licantropia"
-			  				  	]
-			  	},
-			  	
-			  	mutante18: {
-			  				  	numero: 18,
-			  				  	foto: "./assets/ramon.jpeg",
-			  				  	nome: "Ramon",
-			  				  	
-			  				  	nomeCompleto: "Ramon Fusily",
-        poderes: [
-            "Genes de Lobo",
-			  				  	"Presas Afiadas",
-			  				  	"Sentidos Aguçados",
-			  				  	"Força Sobre-Humana",
-			  				  	"Transmissão de Licantropia"
-			  				  	]
-			  	},
-			  	
-			  	mutante19: {
-			  				  	numero: 19,
-			  				  	foto: "./assets/eric.jpeg",
-			  				  	nome: "Eric",
-			  				  	nomeCompleto: "Eric Fusily",
-        poderes: [
-            "Genes de Lobo",
-			  				  	"Presas Afiadas",
-			  				  	"Sentidos Aguçados",
-			  				  	"Força Sobre-Humana",
-			  				  	"Transmissão de Licantropia"
-			  				  	]
-			  	},
-			  	
-			  	mutante20: {
-			  				  	numero: 20,
-			  				  	foto: "./assets/felina.jpg",
-			  				  	nome: "Felina",
-			  				  	nomeCompleto: "Mulher-Pantera",
-        poderes: [
-            "Presas e Garras Afiadas",
-			  				  	     "Canibalismo",
-			  				  	"Sentidos Aguçados",
-			  				  	"Agilidade de Pantera",
-			  				  	"Super Velocidade de Pantera",
-			  				  	"Transmissão de Felinismo"
-			  				  	]
-			  	},
-			  	
-			  	mutante21: {
-			  				  	numero: 21,
-			  				  	foto: "./assets/gor.jpeg",
-			  				  	nome: "Gór",
-			  				  	nomeCompleto: "Górgorna Mayer",
-        poderes: [
-            "Hipnose",
-			  				  	]
-			  	},
+  mutante01: {
+    numero: 1,
+    foto: "./assets/vlado.jpeg",
+    nome: "Vlado",
+    nomeCompleto: "Vladmir Pereira",
+    poderes: [
+      "Presas Afiadas",
+      "Hematofagia",
+      "Sentidos Aguçados",
+      "Visão Noturna",
+      "Transmissão de Vampirismo",
+      "Ecolocalização",
+      "Agilidade de Morcego",
+    ],
+  },
+  mutante02: {
+    numero: 2,
+    foto: "./assets/chris.JPG",
+    nome: "Chris",
+    nomeCompleto: "Cristiano Pena",
+    poderes: [
+      "Super Sensibilidade",
+      "Telecinese",
+      "Sentidos Aguçados",
+      "Telepatia",
+      "Lingüistica Animal",
+      "Aerocinese",
+      "Psicocinese",
+      "Controle de Gravidade",
+      "Magnetocinese",
+      "Indução de Dor",
+      "Ataques Mentais",
+      "Regeneração Celular",
+      "Imortalidade",
+      "Materialização",
+      "Fotocinese",
+      "Indução de Morte",
+      "Absorção de Força Vital",
+      "Indução de Paralisia",
+      "Manipulação da Idade",
+      "Transmutação",
+      "Controle Climático",
+      "Anulação de Poderes",
+      "Super Resistência",
+      "Pirocinese",
+      "Envelhecimento Precoce",
+    ],
+  },
+  mutante03: {
+    numero: 3,
+    foto: "./assets/agata.jpg",
+    nome: "Ágata",
+    nomeCompleto: "Ágata Magalhães",
+    poderes: ["Garras", "Super Visão"],
+  },
+  mutante04: {
+    numero: 4,
+    foto: "./assets/metamorfo.jpeg",
+    nome: "Metamorfo",
+    nomeCompleto: "Mateus Morpheus Mayer",
+    poderes: ["Invisibilidade", "Metamorfismo Corporal"],
+  },
+
+  mutante05: {
+    numero: 5,
+    foto: "./assets/aquiles.jpeg",
+    nome: "Aquiles",
+    nomeCompleto: "Aquiles Magalhães",
+    poderes: [
+      "Super Velocidade",
+      "Regeneração Celular",
+      "Imunidade a Contaminantes",
+      "Super Agilidade",
+      "Super Resistência",
+    ],
+  },
+  mutante06: {
+    numero: 6,
+    foto: "./assets/clara.jpeg",
+    nome: "Clara",
+    nomeCompleto: "Clara Menezes",
+    poderes: [
+      "Cura",
+      "Super Sensibilidade",
+      "Hipnose Animal",
+      "Luminescência",
+      "Transmissão de Sentimentos",
+    ],
+  },
+  mutante07: {
+    numero: 7,
+    foto: "./assets/angela.jpeg",
+    nome: "Ângela",
+    nomeCompleto: "Ângela Menezes",
+    poderes: ["Asas", "Dom da Calma", "Super Sensibilidade"],
+  },
+  mutante08: {
+    numero: 8,
+    foto: "./assets/tatiana.jpg",
+    nome: "Tati",
+    nomeCompleto: "Tatiana Montenegro",
+    poderes: [
+      "Telecinese",
+      "Pirocinese",
+      "Ondas Mentais",
+      "Telepatia",
+      "Tortura mental",
+      "Raios de energia",
+      "Magnetocinese",
+      "Grito Super Sônico",
+      "Fotocinese",
+      "Campos de Força",
+      "Escudos Mentais",
+      "Atomocinese",
+      "Imobilização",
+      "Geocinese",
+      "Rastreamento",
+      "Sentido de perigo",
+      "Areocinese",
+      "Detectação de Energia",
+    ],
+  },
+  mutante09: {
+    numero: 9,
+    foto: "./assets/toni.jpeg",
+    nome: "Toni",
+    nomeCompleto: "Antônio Mayer",
+    poderes: [
+      "Super Agilidade",
+      "Super Reflexos",
+      "Super Equilíbrio",
+      "Super Resistência",
+    ],
+  },
+  mutante10: {
+    numero: 10,
+    foto: "./assets/janete.jpeg",
+    nome: "Janete",
+    nomeCompleto: "Janete Fontes Martinelli",
+    poderes: [
+      "Premonição",
+      "Clarividência",
+      "Raios Oculares",
+      "Controle Mental",
+      "Controle Mental",
+      "Super Sensibilidade",
+      "Biocinese",
+      "Neutralização de Poderes",
+      "Reversão de Poderes",
+      "Manipulação Vital",
+    ],
+  },
+  mutante11: {
+    numero: 11,
+    foto: "./assets/perpetua.jpeg",
+    nome: "Perpétua",
+    nomeCompleto: "Perpétua Salvador da Silva",
+    poderes: ["Eletrocinese", "Campo de Energia Elétrica"],
+  },
+  mutante12: {
+    numero: 12,
+    foto: "./assets/pachola.jpeg",
+    nome: "Pachola",
+    nomeCompleto: "Paulo Pachola",
+    poderes: [
+      "Genes de Lobo",
+      "Presas Afiadas",
+      "Sentidos Aguçados",
+      "Força Sobre-Humana",
+      "Transmissão de Licantropia",
+    ],
+  },
+  mutante13: {
+    numero: 13,
+    foto: "./assets/maria.jpeg",
+    nome: "Maria Mayer",
+    nomeCompleto: "Maria Beatriz Montenegro",
+    poderes: [
+      "Super força",
+      "Super agilidade",
+      "Resistência evoluída",
+      "Habilidades acrobáticas",
+      "Intangibilidade",
+      "Sensibilidade sobre-humana",
+      "Manipulação de Feromônios",
+      "Absorção de poderes",
+      "Cariocinese",
+      "Espinhos",
+      "Magnetocinese",
+      "Eletrocinese",
+    ],
+  },
+  mutante14: {
+    numero: 14,
+    foto: "./assets/vava.jpeg",
+    nome: "Vavá",
+    nomeCompleto: "Valfredo Pachola",
+    poderes: [
+      "Genes de Lobo",
+      "Transmissão de Licantropia",
+      "Super força",
+      "Presas Afiadas",
+      "Sentidos Aguçados",
+      "Super Agilidade",
+    ],
+  },
+
+  mutante15: {
+    numero: 15,
+    foto: "./assets/lucas.jpeg",
+    nome: "Lucas",
+    nomeCompleto: "Lucas Fontes Martinelli",
+    poderes: ["Ler pensamentos"],
+  },
+
+  mutante16: {
+    numero: 16,
+    foto: "./assets/cleo.jpeg",
+    nome: "Cléo",
+    nomeCompleto: "Cléo Mayer",
+    poderes: ["Sobrevivência", "Projeção Astral", "Petrificação"],
+  },
+  mutante17: {
+    numero: 17,
+    foto: "./assets/dr-cesar.jpeg",
+    nome: "Dr. césar",
+    nomeCompleto: "César Rubicão",
+    poderes: [
+      "Genes de Lobo",
+      "Presas Afiadas",
+      "Sentidos Aguçados",
+      "Força Sobre-Humana",
+      "Transmissão de Licantropia",
+    ],
+  },
+
+  mutante18: {
+    numero: 18,
+    foto: "./assets/ramon.jpeg",
+    nome: "Ramon",
+
+    nomeCompleto: "Ramon Fusily",
+    poderes: [
+      "Genes de Lobo",
+      "Presas Afiadas",
+      "Sentidos Aguçados",
+      "Força Sobre-Humana",
+      "Transmissão de Licantropia",
+    ],
+  },
+
+  mutante19: {
+    numero: 19,
+    foto: "./assets/eric.jpeg",
+    nome: "Eric",
+    nomeCompleto: "Eric Fusily",
+    poderes: [
+      "Genes de Lobo",
+      "Presas Afiadas",
+      "Sentidos Aguçados",
+      "Força Sobre-Humana",
+      "Transmissão de Licantropia",
+    ],
+  },
+
+  mutante20: {
+    numero: 20,
+    foto: "./assets/felina.jpg",
+    nome: "Felina",
+    nomeCompleto: "Mulher-Pantera",
+    poderes: [
+      "Presas e Garras Afiadas",
+      "Canibalismo",
+      "Sentidos Aguçados",
+      "Agilidade de Pantera",
+      "Super Velocidade de Pantera",
+      "Transmissão de Felinismo",
+    ],
+  },
+
+  mutante21: {
+    numero: 21,
+    foto: "./assets/gor.jpeg",
+    nome: "Gór",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante22: {
+    numero: 22,
+    foto: "./assets/eugenio.webp",
+    nome: "Eugênio",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+  mutante23: {
+    numero: 23,
+    foto: "./assets/dr-julia.jpg",
+    nome: "Júlia Zaccarias",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante24: {
+    numero: 24,
+    foto: "./assets/lupi.webp",
+    nome: "Lupi",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante25: {
+    numero: 25,
+    foto: "./assets/lupo.JPG",
+    nome: "Lupo",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante26: {
+    numero: 26,
+    foto: "./assets/juanita.jpg",
+    nome: "Juanita",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante27: {
+    numero: 27,
+    foto: "./assets/fernando.webp",
+    nome: "Fernando",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante28: {
+    numero: 28,
+    foto: "./assets/paola.webp",
+    nome: "Paola",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante29: {
+    numero: 29,
+    foto: "./assets/fabiana.webp",
+    nome: "Fabiana",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante30: {
+    numero: 30,
+    foto: "./assets/helio.jpeg",
+    nome: "Hélio",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante32: {
+    numero: 31,
+    foto: "./assets/taveira.jpeg",
+    nome: "Taveira",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+
+  mutante32: {
+    numero: 32,
+    foto: "./assets/dino.jpeg",
+    nome: "Dino",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+
+  mutante33: {
+    numero: 33,
+    foto: "./assets/dr-beatriz.jpeg",
+    nome: "Dr.Beatriz",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
+  mutante34: {
+    numero: 34,
+    foto: "./assets/pepe.webp",
+    nome: "Pepe",
+    nomeCompleto: "Górgorna Mayer",
+    poderes: ["Hipnose"],
+  },
 };
